401acbde053b0b292a27e2861d19cd7d3e8128d6
Erik Haartmans <erik.haartmans@polteq.com>
2024-01-05 12:53:54 +0100

    added playwright java browserfactory

---
+++ playwright/java/lib/BrowserFactory.java
@@ -0,0 +1,32 @@
+package lib;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Playwright;
+
+public class BrowserFactory {
+
+    private static final boolean headless = false;
+    private static final double slowMo = 0;
+
+    public static Browser createBrowser(Playwright playwright, BrowserName browserName) {
+        return switch (browserName) {
+            case CHROMIUM -> createChromiumBrowser(playwright);
+            case FIREFOX -> createFirefoxBrowser(playwright);
+            case WEBKIT -> createWebkitBrowser(playwright);
+        };
+    }
+
+    private static Browser createWebkitBrowser(Playwright playwright) {
+        return playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(headless).setSlowMo(slowMo));
+    }
+
+    private static Browser createFirefoxBrowser(Playwright playwright) {
+        return playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(headless).setSlowMo(slowMo));
+    }
+
+    private static Browser createChromiumBrowser(Playwright playwright) {
+        return playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(headless).setSlowMo(slowMo));
+    }
+
+}
---
+++ playwright/java/lib/BrowserName.java
@@ -0,0 +1,7 @@
+package lib;
+
+public enum BrowserName {
+    CHROMIUM,
+    FIREFOX,
+    WEBKIT
+}
