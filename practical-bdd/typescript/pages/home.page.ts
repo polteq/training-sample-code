b424105a609f65a49bd95f7c60b416c189e0b2cf
Marco Maes <49234445+marcomaes@users.noreply.github.com>
2026-06-28 16:07:23 +0200

    Fix import path for BasePage in home.page.ts

--- practical-bdd/typescript/pages/home.page.ts
+++ practical-bdd/typescript/pages/home.page.ts
@@ -1,5 +1,5 @@
 import { Page } from '@playwright/test';
-import { BasePage } from './base-page';
+import { BasePage } from './base.page';
 
 export class HomePage extends BasePage {
   private readonly myAccountButton = this.page.locator('li.my-account a');
