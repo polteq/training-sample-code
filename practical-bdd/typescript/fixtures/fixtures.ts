6fb03fa104c69d584275055d477182fab0ff3bfd
Marco Maes <49234445+marcomaes@users.noreply.github.com>
2026-06-28 16:06:17 +0200

    Create fixtures.ts

---
+++ practical-bdd/typescript/fixtures/fixtures.ts
@@ -0,0 +1,24 @@
+import { test as base, createBdd } from 'playwright-bdd';
+import { HomePage } from '../pages/home-page';
+import { MyAccountPage } from '../pages/my-account-page';
+import { ContactPage } from '../pages/contact-page';
+
+type Fixtures = {
+  homePage: HomePage;
+  myAccountPage: MyAccountPage;
+  contactPage: ContactPage;
+};
+
+export const test = base.extend<Fixtures>({
+  homePage: async ({ page }, use) => {
+    await use(new HomePage(page));
+  },
+  myAccountPage: async ({ page }, use) => {
+    await use(new MyAccountPage(page));
+  },
+  contactPage: async ({ page }, use) => {
+    await use(new ContactPage(page));
+  },
+});
+
+export const { Given, When, Then, Before, After } = createBdd(test);
