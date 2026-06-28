7fdb8a87c7273ac7c22bc540c945cb3a4cc347f4
Marco Maes <49234445+marcomaes@users.noreply.github.com>
2026-06-28 16:08:03 +0200

    Fix import path for BasePage in my-account.page.ts

--- practical-bdd/typescript/pages/my-account.page.ts
+++ practical-bdd/typescript/pages/my-account.page.ts
@@ -1,5 +1,5 @@
 import { Page } from '@playwright/test';
-import { BasePage } from './base-page';
+import { BasePage } from './base.page';
 
 export class MyAccountPage extends BasePage {
   private readonly emailField = this.page.locator('input#username');
